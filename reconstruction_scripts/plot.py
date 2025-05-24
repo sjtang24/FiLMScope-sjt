37574fc447c5eabac5149f2f767fd724dc02897d
Steven Tang <sjt49@duke.edu>
2025-05-23 23:27:14 -0400

    added camera arrangement experiments

--- reconstruction_scripts/plot.py
+++ reconstruction_scripts/plot.py
@@ -10,33 +10,38 @@ import pandas as pd
 time = input("Enter the time:")
 sample_name = "knuckle_video"
 filename = f'timings/video-reconstruction_{sample_name}.npy'
-times = np.load(filename)
-startup_times = np.load(f"timings/setup_{sample_name}.npy")
-#print(times)
+times = np.load(filename, allow_pickle = True).item()
+startup_times = np.load(f"timings/setup_{sample_name}.npy", allow_pickle = True).item()
 downsample_factors = np.array([1, 2, 4, 8, 16, 32])
 
 # total runtime duration
 fig, (ax1, ax2) = plt.subplots(1, 2)
 fig.suptitle(f"Downsampling Speedup for Sample {sample_name}")
-ax1.plot(downsample_factors, np.sum(times, axis = 1), linestyle='-')
+for arrangement in times:
+    ax1.plot(downsample_factors, np.sum(times[arrangement], axis = 1), linestyle='-', label = arrangement)
+    ax2.errorbar(downsample_factors, np.mean(times[arrangement], axis = 1), yerr=np.std(times[arrangement], axis = 1), label = arrangement)
 ax1.set_xscale('log', base = 2)
 ax1.set_xlabel('Downsampling Factors')
 ax1.set_title('Total Runtime (in seconds)')
-
-# mean runtime duration
-ax2.errorbar(downsample_factors, np.mean(times, axis = 1), yerr=np.std(times, axis = 1))
 ax2.set_xscale('log', base = 2)
 ax2.set_xlabel('Downsampling Factors')
 ax2.set_title('Runtime per Frame (in seconds)')
+ax2.legend()
 fig.savefig(f"plots/{time}/timing/{sample_name}/runtime.png")
 
-plt.figure()
-plt.plot(downsample_factors, startup_times)
-plt.title("Startup Time, by Downsampling Factors")
-plt.xlabel('Downsampling Factors')
-plt.ylabel("Time (in seconds)")
-plt.savefig(f"plots/{time}/timing/{sample_name}/setup.png")
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)  # Create an Axes object
 
+for arrangement in times:
+    ax.plot(downsample_factors, startup_times[arrangement], label=arrangement)
+
+ax.set_xscale('log', base=2)
+ax.set_title("Startup Time, by Downsampling Factors")
+ax.set_xlabel('Downsampling Factors')
+ax.set_ylabel("Time (in seconds)")
+ax.legend()
+fig.savefig(f"plots/{time}/timing/{sample_name}/setup.png")
+"""
 directory = Path(f"plots/{time}/heightmap/{sample_name}")
 heightmaps_filenames = {downsample : sorted(file for file in directory.glob(f"*ds{downsample}.png")) for downsample in downsample_factors}
 
@@ -48,34 +53,44 @@ heightmaps_filenames = {downsample : sorted(file for file in directory.glob(f"*d
 # for factor in downsample_factors:
 #     images = [imageio.imread(f) for f in heightmaps_filenames[factor]]
 #     imageio.mimsave(f'animations/animation_{factor}.gif', images, duration=0.2, loop = 0)  # duration is per frame in seconds
-
+"""
 with open("rmses.pkl", 'rb') as rmse_file:
     rmses = pickle.load(rmse_file)
 
 with open("ssim.pkl", 'rb') as ssim_file:
     ssims = pickle.load(ssim_file)
 
-rmses_df = pd.DataFrame.from_dict(rmses, orient = 'columns')
-ssims_df = pd.DataFrame.from_dict(ssims, orient = 'columns')
 
-fig = plt.figure()
-for col in rmses_df.columns:
-    plt.plot(rmses_df.index, rmses_df[col], label=f'{col}')
+rmses_df_dict = {
+    arrangement : pd.DataFrame.from_dict(rmses[arrangement], orient = 'columns') for arrangement in rmses
+}
 
-plt.title("RMSE between Gold Standard and 3D Reconstructions\n(Each line corresponds to a downsampling factor)")
-plt.xlabel("Iteration")
-plt.ylabel("RMSE")
-plt.legend(title="Downsampling Factors")
-plt.tight_layout()
-fig.savefig(f"rmses.png")
+ssims_df_dict = {
+    arrangement : pd.DataFrame.from_dict(ssims[arrangement], orient = 'columns') for arrangement in ssims
+}
+ 
 
-fig = plt.figure()
-for col in ssims_df.columns:
-    plt.plot(ssims_df.index, ssims_df[col], label=f'{col}')
+for arrangement in times:
+    rmses_df = rmses_df_dict[arrangement]
+    ssims_df = ssims_df_dict[arrangement]
+    fig = plt.figure()
+    for col in rmses_df.columns:
+        plt.plot(rmses_df.index, rmses_df[col], label=f'{col}')
 
-plt.title("SSIM between Gold Standard and 3D Reconstructions\n(Each line corresponds to a downsampling factor)")
-plt.xlabel("Iteration")
-plt.ylabel("SSIM")
-plt.legend(title="Downsampling Factors")
-plt.tight_layout()
-fig.savefig(f"ssims.png")
+    plt.title("RMSE between Gold Standard and 3D Reconstructions\n(Each line corresponds to a downsampling factor)")
+    plt.xlabel("Iteration")
+    plt.ylabel("RMSE")
+    plt.legend(title="Downsampling Factors")
+    plt.tight_layout()
+    fig.savefig(f"plots/{time}/rmses_{arrangement}.png")
+
+    fig = plt.figure()
+    for col in ssims_df.columns:
+        plt.plot(ssims_df.index, ssims_df[col], label=f'{col}')
+
+    plt.title("SSIM between Gold Standard and 3D Reconstructions\n(Each line corresponds to a downsampling factor)")
+    plt.xlabel("Iteration")
+    plt.ylabel("SSIM")
+    plt.legend(title="Downsampling Factors")
+    plt.tight_layout()
+    fig.savefig(f"plots/{time}/ssims_{arrangement}.png")
--- reconstruction_scripts/video_reconstruction_exp.py
+++ reconstruction_scripts/video_reconstruction_exp.py
@@ -19,7 +19,6 @@ from matplotlib.gridspec import GridSpec
 import numpy as np
 import torch 
 from datetime import datetime
-import time 
 import pickle
 
 use('Agg') # display in matplotlib
@@ -29,7 +28,7 @@ print(torch.cuda.device_count())
 sample_name = "knuckle_video"
 gpu_number = '0' 
 if torch.cuda.is_available():
-    print(f"GPUs Available: {torch.cuda.device_count()} (Using 2)")
+    print(f"GPUs Available: {torch.cuda.device_count()} (Using 1)")
     gpu_number = '1' 
 else:
     print("No GPUs available!")
@@ -59,147 +58,159 @@ except FileExistsError:
 # TODO Choose hyperparameters to loop through
 frame_numbers = np.asarray([420])
 downsample_factors = [1, 2, 4, 8, 16, 32]
+num_cameras = 48
+camera_arrangements = {
+    'all_cameras' : np.arange(0, num_cameras).tolist(),
+    'wide_sparse' : [6, 10, 20, 30, 34],
+    'narrow_sparse' : [14, 16, 20, 26, 28],
+    '2x2 grid' : [20, 21, 26, 27],
+    '4x4 grid' : [13, 14, 15, 16, 19, 20, 21, 22, 25, 26, 27, 28, 31, 32, 33, 34]
+}
 
 #downsample_factors = [downsample]
-times = np.zeros((len(downsample_factors), frame_numbers.size))
-setup_times = np.zeros(len(downsample_factors))
+times = {key : np.zeros((len(downsample_factors), frame_numbers.size)) for key in camera_arrangements}
+setup_times = {key : np.zeros(len(downsample_factors)) for key in camera_arrangements}
 gold_standards = None
 RMSE = None
 
-for downsample_i, downsample in enumerate(downsample_factors):
-    # print(f"Timing downsampling factor of {downsample}...")
-    config_dict = generate_config_dict(sample_name=sample_name, gpu_number=gpu_number, downsample=downsample,
-                                    camera_set="all", use_neptune=use_neptune,
-                                    log_description=log_description, frame_number=frame_numbers[0],
-                                    run_args={"iters": 150, "batch_size": 12, "num_depths": 32,
-                                                "display_freq": 25})
-    run_manager = RunManager(config_dict)
-    setup_times[downsample_i] = run_manager.setup_time
-    print(run_manager.setup_time)
-    indices = run_manager.dataset.full_crops[20]
+for arrangement_i, arrangement in enumerate(camera_arrangements):
+    print(f"Arrangement: {arrangement}")
+    for downsample_i, downsample in enumerate(downsample_factors):
+        # print(f"Timing downsampling factor of {downsample}...")
+        config_dict = generate_config_dict(sample_name=sample_name, gpu_number=gpu_number, downsample=downsample,
+                                        camera_set="custom", use_neptune=use_neptune,
+                                        custom_image_numbers=camera_arrangements[arrangement],
+                                        log_description=log_description, frame_number=frame_numbers[0],
+                                        run_args={"iters": 150, "batch_size": 12, "num_depths": 32,
+                                                    "display_freq": 25})
+        run_manager = RunManager(config_dict)
+        setup_times[arrangement][downsample_i] = run_manager.setup_time
+        #print(run_manager.setup_time)
+        indices = run_manager.dataset.full_crops[20]
 
-    # I assume here that negative indices denote padding so we start with -startx or -starty,
-    # and we multiply by the downsample to get the cropping information for the upsampled images 
-    # print(f"Downsampling Factor {downsample}")
-    startx, endx, starty, endy = tuple([
-        -crop_coords if crop_coords < 0 else crop_coords 
-        for crop_coords in indices
-    ])
-    sx, sy = startx * downsample, starty * downsample
-    ex, ey = endx * downsample, endy * downsample
-    
-    # perform reconstruction
-    # if convergence happens quickly for some frames, 
-    # follow prompts in the terminal to adjust # iterations used
-    # or to manually move on to the next frame
-    run_args = config_dict["run_args"]
-    if downsample == 1: 
-        gold_standards = {fn : None for fn in frame_numbers}
-        RMSE = {ds : [] for ds in downsample_factors if ds > 1}
-        SSIM = {ds : [] for ds in downsample_factors if ds > 1}
-    
-    for frame_number_i, frame_number in enumerate(frame_numbers):
-        #print("")
-        #print("enter 'iters: {number}' to adjust # iterations for the NEXT frame")
-        #print("OR enter 'continue' to move on to the next frame")
-        #print("")
-
-        # useful to update description for each frame
-        run_manager.config_dict["log_description"] = f"frame {frame_number} " + log_description
-        run_manager.swap_frames(frame_number)
+        # I assume here that negative indices denote padding so we start with -startx or -starty,
+        # and we multiply by the downsample to get the cropping information for the upsampled images 
+        # print(f"Downsampling Factor {downsample}")
+        startx, endx, starty, endy = tuple([
+            -crop_coords if crop_coords < 0 else crop_coords 
+            for crop_coords in indices
+        ])
+        sx, sy = startx * downsample, starty * downsample
+        ex, ey = endx * downsample, endy * downsample
         
-        losses = []
-        #print(f"Starting frame {frame_number}")
-        # disable = True removes the progress bar 
-        duration = 0
-        for i in tqdm(range(run_args["iters"]), disable = downsample > 1):
-            done = i == run_args["iters"] - 1
-            display = i > 0 and i % run_args["display_freq"] == 0
-            log = done or display 
+        # perform reconstruction
+        # if convergence happens quickly for some frames, 
+        # follow prompts in the terminal to adjust # iterations used
+        # or to manually move on to the next frame
+        run_args = config_dict["run_args"]
+        if downsample == 1 and arrangement == 'all_cameras': 
+            gold_standards = {fn : None for fn in frame_numbers}
+            RMSE = {cams : {ds : [] for ds in downsample_factors if ds > 1} for cams in camera_arrangements}
+            SSIM = {cams : {ds : [] for ds in downsample_factors if ds > 1} for cams in camera_arrangements}
+        
+        for frame_number_i, frame_number in enumerate(frame_numbers):
+            #print("")
+            #print("enter 'iters: {number}' to adjust # iterations for the NEXT frame")
+            #print("OR enter 'continue' to move on to the next frame")
+            #print("")
 
-            start_time = time.perf_counter()
-            _, _, _, outputs, loss_values = run_manager.run_epoch(i, log and use_neptune)
-            duration += (time.perf_counter() - start_time)
-            losses.append(float(loss_values["total"]))
+            # useful to update description for each frame
+            run_manager.config_dict["log_description"] = f"frame {frame_number} " + log_description
+            run_manager.swap_frames(frame_number)
             
-            # check here for terminal inputs to move on to next frame if desired
-            # or adjust the number of iterations being used
-            # if select.select([sys.stdin], [], [], 0.1)[0]:
-            #     user_input = sys.stdin.readline().strip()
+            losses = []
+            #print(f"Starting frame {frame_number}")
+            # disable = True removes the progress bar 
+            duration = 0
+            for i in tqdm(range(run_args["iters"]), disable = downsample > 1):
+                done = i == run_args["iters"] - 1
+                display = i % run_args["display_freq"] == 0
+                log = done or display 
 
-            #     command_parts = user_input.split(": ", 1)
-            #     if len(command_parts) == 2:
-            #         command, value = command_parts
-            #         if command.lower() == "iters":
-            #             print(f"switching to {value} iters on next frame")
-            #             run_args["iters"] = int(value)
-            #     elif user_input.lower() == "continue":
-            #         print("moving on to next frame...")
-            #         break
+                start_time = time.perf_counter()
+                _, _, _, outputs, loss_values = run_manager.run_epoch(i, log and use_neptune)
+                duration += (time.perf_counter() - start_time)
+                losses.append(float(loss_values["total"]))
+                
+                # check here for terminal inputs to move on to next frame if desired
+                # or adjust the number of iterations being used
+                # if select.select([sys.stdin], [], [], 0.1)[0]:
+                #     user_input = sys.stdin.readline().strip()
 
-            # this block can be edited to save/log in another way
-            # this simply displays some outputs with matplotlib
-            heightmap = outputs["depth"].detach().cpu().squeeze().numpy()
+                #     command_parts = user_input.split(": ", 1)
+                #     if len(command_parts) == 2:
+                #         command, value = command_parts
+                #         if command.lower() == "iters":
+                #             print(f"switching to {value} iters on next frame")
+                #             run_args["iters"] = int(value)
+                #     elif user_input.lower() == "continue":
+                #         print("moving on to next frame...")
+                #         break
 
-            if done:
-                if downsample == 1: 
-                    gold_standards[frame_number] = heightmap
-                times[downsample_i, frame_number_i] = duration
+                # this block can be edited to save/log in another way
+                # this simply displays some outputs with matplotlib
+                heightmap = outputs["depth"].detach().cpu().squeeze().numpy()
 
-            # TODO change this to linear interpolation
-            #expanded_heightmap = heightmap.repeat_interleave(downsample, dim = 0).repeat_interleave(downsample, dim = 1)[
-            #    sx:ex, sy:ey
-            #]
-            expanded_heightmap = zoom(heightmap, zoom=downsample, order=1)[sx:ex, sy:ey]  # order=1 means linear interpolation   
-            reference = run_manager.reference_image.cpu().squeeze()[startx:endx, starty:endy]
+                if done:
+                    if downsample == 1 and arrangement == 'all_cameras': 
+                        gold_standards[frame_number] = heightmap
+                    times[arrangement][downsample_i, frame_number_i] = duration
 
-            if downsample > 1:
-                RMSE[downsample].append(
-                    int(np.sqrt(
-                        np.sum(
-                            np.square(
-                                expanded_heightmap - gold_standards[frame_number]
+                # TODO change this to linear interpolation
+                #expanded_heightmap = heightmap.repeat_interleave(downsample, dim = 0).repeat_interleave(downsample, dim = 1)[
+                #    sx:ex, sy:ey
+                #]
+                expanded_heightmap = zoom(heightmap, zoom=downsample, order=1)[sx:ex, sy:ey]  # order=1 means linear interpolation   
+                reference = run_manager.reference_image.cpu().squeeze()[startx:endx, starty:endy]
+
+                if downsample > 1:
+                    RMSE[arrangement][downsample].append(
+                        float(np.sqrt(
+                            np.sum(
+                                np.square(
+                                    expanded_heightmap - gold_standards[frame_number]
+                                )
                             )
-                        )
-                    ))
-                )
-                mssim = structural_similarity(
-                    expanded_heightmap, gold_standards[frame_number], full=False
-                )
-                SSIM[downsample].append(mssim)
+                        ))
+                    )
+                    mssim = structural_similarity(
+                        expanded_heightmap, gold_standards[frame_number], full=False
+                    )
+                    SSIM[arrangement][downsample].append(mssim)
 
-            if log and not use_neptune and downsample > 1:                
-                fig = plt.figure()
-                width_ratios = [1, 1, 1]
-                gs = GridSpec(1, 3, width_ratios=width_ratios)
-                ax0 = fig.add_subplot(gs[0])
-                ax1 = fig.add_subplot(gs[1])
-                ax2 = fig.add_subplot(gs[2])
-                ax0.axis('off')
-                ax1.axis('off')
-                ax2.axis('off')
+                if log and not use_neptune:                
+                    fig = plt.figure()
+                    width_ratios = [1, 1, 1]
+                    gs = GridSpec(1, 3, width_ratios=width_ratios)
+                    ax0 = fig.add_subplot(gs[0])
+                    ax1 = fig.add_subplot(gs[1])
+                    ax2 = fig.add_subplot(gs[2])
+                    ax0.axis('off')
+                    ax1.axis('off')
+                    ax2.axis('off')
 
-                ax2.imshow(expanded_heightmap, cmap = 'turbo')
-                ax1.imshow(gold_standards[frame_number], cmap='turbo')
-                ax0.imshow(reference, cmap='gray')
-                ax2.set_title(f"Reconstructed")
-                ax1.set_title(f"Gold Standard")
-                ax0.set_title(f"Reference Image")
-                plt.savefig(f"plots/{time_at_start}/heightmap/{sample_name}/{frame_number}_ds{downsample}.png")
-                #plt.show()
+                    ax2.imshow(expanded_heightmap, cmap = 'turbo')
+                    if gold_standards != None and gold_standards[frame_number] is not None:
+                        ax1.imshow(gold_standards[frame_number], cmap='turbo')
+                    ax0.imshow(reference, cmap='gray')
+                    ax2.set_title(f"Reconstructed")
+                    ax1.set_title(f"Gold Standard")
+                    ax0.set_title(f"Reference Image")
+                    plt.savefig(f"plots/{time_at_start}/heightmap/{sample_name}/{frame_number}_ds{downsample}_{arrangement}.png")
+                    #plt.show()
 
-                plt.figure()
-                plt.plot(losses)
-                plt.title(f"losses, frame {frame_number}, downsampling {downsample}")
-                plt.xlabel("iteration")
-                plt.ylabel("loss")
-                plt.savefig(f"plots/{time_at_start}/losses/{sample_name}/{frame_number}_ds{downsample}.png")
-                #plt.show()
+                    plt.figure()
+                    plt.plot(losses)
+                    plt.title(f"losses, frame {frame_number}, downsampling {downsample}, {arrangement}")
+                    plt.xlabel("iteration")
+                    plt.ylabel("loss")
+                    plt.savefig(f"plots/{time_at_start}/losses/{sample_name}/{frame_number}_ds{downsample}_{arrangement}.png")
+                    #plt.show()
 
-        # print(f"Execution runtime for frame: {frame_duration}s")
-    #print(f"Total runtime for downsample factor {downsample}: {np.sum(times[downsample_i, :])}s")
-    #print(f"Runtime/Frame for downsample factor {downsample}: {np.mean(times[downsample_i, :])}s +/- {np.std(times[downsample_i, :])}")
-    print(RMSE)
+            # print(f"Execution runtime for frame: {frame_duration}s")
+        #print(f"Total runtime for downsample factor {downsample}: {np.sum(times[downsample_i, :])}s")
+        #print(f"Runtime/Frame for downsample factor {downsample}: {np.mean(times[downsample_i, :])}s +/- {np.std(times[downsample_i, :])}")
+        print(RMSE)
 np.save(f"timings/video-reconstruction_{sample_name}.npy", times)
 np.save(f"timings/setup_{sample_name}.npy", setup_times)
 
